fe01e267ca79b0cc394f47f8c7988a6bc708349a
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-08 18:26:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
         receiver.balance += self.amount
         self.status = "complete"
       else 
-     self.status = "Transaction rejected. Please check your account balance."
+     "Transaction rejected. Please check your account balance."
       end
     end
   
