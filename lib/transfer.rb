51f53d45549470f147e9387ab38eeaa3e7c407f8
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-08 18:13:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
         sender.balance -= self.amount  
         receiver.balance += self.amount
       else
+        self.status != "pending"
         self.status == "complete"
     end
   end
