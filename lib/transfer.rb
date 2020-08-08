03dc89991c054091899f6add6e63a55bbae0b952
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-08 18:32:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,11 +11,7 @@ class Transfer
   end
   
   def valid? 
-    if sender.valid? && receiver.valid? 
-      true 
-    else 
-      false 
-    end
+     sender.valid? && receiver.valid? 
   end
   
   def execute_transaction
