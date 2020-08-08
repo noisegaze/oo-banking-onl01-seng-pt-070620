f2ab06430daef7a5ab2160adb721af9edf7aaba5
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-08 18:11:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,7 @@ class Transfer
   def execute_transaction
     if  self.status == "pending"
         sender.balance -= self.amount  
-        receiver.balance += self.amount 
-        && self.status == "complete"
+        receiver.balance += self.amount  && self.status == "complete"
     end
   end
   
